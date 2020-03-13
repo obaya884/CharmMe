e45aaba3a2e46166080636300c04426a79569887
obaya884 <taku3.884@gmail.com>
2020-03-14 03:40:14 +0900

    finish implementing TimePickerDialog Fragment, convert time string to time int

--- app/src/main/java/app/takumi/obayashi/charmme/TimePickerFragment.kt
+++ app/src/main/java/app/takumi/obayashi/charmme/TimePickerFragment.kt
@@ -5,28 +5,17 @@ import android.app.TimePickerDialog
 import android.os.Bundle
 import android.widget.TimePicker
 import androidx.fragment.app.DialogFragment
-import kotlinx.android.synthetic.main.activity_add.*
-import java.text.SimpleDateFormat
-import java.util.*
 
 class TimePickerFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-
-        // Create a new instance of TimePickerDialog and return it
         return TimePickerDialog(
             activity,
-            this,
+            activity as AddActivity?,
             0,
             0,
             true
         )
     }
 
-    override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
-        // Do something with the time chosen by the user
-        val calender = Calendar.getInstance()
-        calender.set(0, 0, 0, hourOfDay, minute)
-        val setTime = SimpleDateFormat("HH:mm").format(calender.time)
-        timeEditText.setText(setTime)
-    }
+    override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {}
 }
